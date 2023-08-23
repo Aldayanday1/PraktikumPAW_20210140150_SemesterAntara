9169ee1c35b3a17ed41786a043da031ae6044767
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 19:46:13 +0700

    Tipe Data Character

---
+++ demo2/src/main/java/com/PraktikumPAW/TipeDataCharacter.java
@@ -0,0 +1,13 @@
+package com.PraktikumPAW;
+
+public class TipeDataCharacter {
+    public static void main(String[] args) {
+        char inisialPertama = 'E';
+        char inisialKedua = 'K';
+        char inisialKetiga = 'O';
+
+        System.out.println("inisialPertama: " + inisialPertama);
+        System.out.println("inisialKedua: " + inisialKedua);
+        System.out.println("inisialKetiga: " + inisialKetiga);
+    }
+}
