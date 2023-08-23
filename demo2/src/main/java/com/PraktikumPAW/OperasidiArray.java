bf06c3284cc31a79cf4a69e127c7f9e2fdaf2301
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 23:36:30 +0700

    Operasi di Array

---
+++ demo2/src/main/java/com/PraktikumPAW/OperasidiArray.java
@@ -0,0 +1,15 @@
+package com.PraktikumPAW;
+
+public class OperasidiArray {
+    public static void main(String[] args) {
+        long[] arraylong = {
+            10, 90, 80, 67, 29
+        };
+
+        arraylong[0] = 100;
+
+        System.out.println(arraylong[0]);
+        System.out.println(arraylong[1]);
+        System.out.println(arraylong.length);
+    }
+}
