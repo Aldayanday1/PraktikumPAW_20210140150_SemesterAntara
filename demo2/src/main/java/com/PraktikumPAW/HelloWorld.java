595b894bd493ac88ae13a21ae0758940b0a5b365
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 23:15:21 +0700

    Variable

--- demo2/src/main/java/com/PraktikumPAW/HelloWorld.java
+++ demo2/src/main/java/com/PraktikumPAW/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.PraktikumPAW;
 
 public class HelloWorld {
+    
+    //method utama (main) yang akan dieksekusi pertama kali
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
+
---
+++ demo2/src/main/java/com/PraktikumPAW/Variable.java
@@ -0,0 +1,15 @@
+package com.PraktikumPAW;
+
+public class Variable {
+    public static void main(String[] args) {
+        String nama;
+        nama = "Aldi";
+
+        int age = 21;
+        String address = "Bantul";
+
+        System.out.println(nama);
+        System.out.println(age);
+        System.out.println(address);
+    }
+}
