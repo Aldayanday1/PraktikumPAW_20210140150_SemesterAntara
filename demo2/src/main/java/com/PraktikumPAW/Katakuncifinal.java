3a79842a26d67f5effe0a7cebf0610cb0764545c
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 23:19:50 +0700

    Kata Kunci Final

---
+++ demo2/src/main/java/com/PraktikumPAW/Katakuncifinal.java
@@ -0,0 +1,13 @@
+package com.PraktikumPAW;
+
+public class Katakuncifinal {
+    public static void main(String[] args) {
+        final String name = "Aldi";
+        var age = 21;
+        var address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+    }
+}
