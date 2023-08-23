d0f1c8c531bbd48aedadfb097406171fedd4678d
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 19:48:27 +0700

    Tipe Data Boolean

---
+++ demo2/src/main/java/com/PraktikumPAW/TipeDataBoolean.java
@@ -0,0 +1,11 @@
+package com.PraktikumPAW;
+
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("Benar: " + benar);
+        System.out.println("Salah: " + salah);
+    }
+}
