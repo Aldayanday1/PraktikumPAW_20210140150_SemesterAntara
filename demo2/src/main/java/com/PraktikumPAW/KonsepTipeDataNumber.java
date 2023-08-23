f6f9365aefd3687d12fb7d997962308fafbcb7a1
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 19:42:29 +0700

    Konsep Tipe Data Number

---
+++ demo2/src/main/java/com/PraktikumPAW/KonsepTipeDataNumber.java
@@ -0,0 +1,20 @@
+package com.PraktikumPAW;
+
+public class KonsepTipeDataNumber {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        int jumlah = a + b;
+        int selisih = a - b;
+        int hasilKali = a * b;
+        int hasilBagi = a / b;
+        int sisaBagi =  a % b;
+
+        System.out.println("Jumlah: " + jumlah);
+        System.out.println("selisih: " + selisih);
+        System.out.println("hasilKali: " + hasilKali);
+        System.out.println("hasilBagi: " + hasilBagi);
+        System.out.println("sisaBagi: " + sisaBagi);
+    }
+}
