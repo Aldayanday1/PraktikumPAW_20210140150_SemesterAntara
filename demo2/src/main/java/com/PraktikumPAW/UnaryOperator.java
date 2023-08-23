063fb92e37fd46ffdc5ef3141d7b89e5e3a26690
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 23:43:55 +0700

    Unary Operator

---
+++ demo2/src/main/java/com/PraktikumPAW/UnaryOperator.java
@@ -0,0 +1,15 @@
+package com.PraktikumPAW;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int d = +100;
+
+        d++; //meningkatkan nilai d = 1
+        System.out.println(d);
+
+        d--; //mengurangi nilai d = 1
+        System.out.println(d);
+
+        System.out.println(!true);
+    }
+}
