939de56031279f96edc45521c64f4510b414f8d3
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-24 01:38:16 +0700

    Else Statement

---
+++ demo2/src/main/java/com/PraktikumPAW/ElseStatement.java
@@ -0,0 +1,15 @@
+package com.PraktikumPAW;
+
+public class ElseStatement {
+    public static void main(String[] args) {
+        var nilai = 75;
+        var absen = 90;
+
+        if (nilai >= 75 && absen >=75) {
+            System.out.println("Anda Lulus");
+        }
+        else {
+            System.out.println("Anda Tidak Lulus");
+        }
+    }
+}
