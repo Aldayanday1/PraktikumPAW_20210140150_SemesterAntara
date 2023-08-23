56af8ee68f6ded58d070914c1c3495d1f44b76b4
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 23:17:54 +0700

    Kata Kunci Variabel

---
+++ demo2/src/main/java/com/PraktikumPAW/Katakuncivar.java
@@ -0,0 +1,14 @@
+package com.PraktikumPAW;
+
+public class Katakuncivar {
+    public static void main(String[] args) {
+        var name = "Aldi";
+
+        var age = 21;
+        var address = "Jogja";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+    }
+}
