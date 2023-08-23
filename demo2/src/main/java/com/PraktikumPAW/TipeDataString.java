e9ffa01f6f7e15d5a26e3e4fea15d386ea51828f
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 19:51:50 +0700

    Tipe Data String

---
+++ demo2/src/main/java/com/PraktikumPAW/TipeDataString.java
@@ -0,0 +1,13 @@
+package com.PraktikumPAW;
+
+public class TipeDataString {
+    public static void main(String[] args) {
+        String namaDepan = "Aldi";
+        String namaBelakang = "Raihan";
+        String namaLengkap = namaDepan + " " + namaBelakang;
+
+        System.out.println("Nama Depan: " + namaDepan);
+        System.out.println("Nama Belakang: " + namaBelakang);
+        System.out.println("Nama Lengkap: " + namaLengkap);
+    }
+}
