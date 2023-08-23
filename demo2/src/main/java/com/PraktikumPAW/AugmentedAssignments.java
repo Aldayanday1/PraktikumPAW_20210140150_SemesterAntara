b05d3a6e7b3bbcf2cd679c6b5da8a3d71e173d85
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 23:41:10 +0700

    Augmented Assignments

---
+++ demo2/src/main/java/com/PraktikumPAW/AugmentedAssignments.java
@@ -0,0 +1,16 @@
+package com.PraktikumPAW;
+
+public class AugmentedAssignments {
+    public static void main(String[] args) {
+        int c = 100;
+
+        c+= 10;
+        System.out.println(c);
+
+        c-= 10;
+        System.out.println(c);
+
+        c*= 10;
+        System.out.println(c);
+    }
+}
