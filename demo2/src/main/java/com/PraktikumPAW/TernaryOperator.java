035d9afddbce90b4a9d1f2b0ed1631ff95567ed4
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-24 02:02:34 +0700

    Ternary Operator

---
+++ demo2/src/main/java/com/PraktikumPAW/TernaryOperator.java
@@ -0,0 +1,11 @@
+package com.PraktikumPAW;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 74; 
+        
+        String ucapan = nilai >= 75 ? "Selamat Anda Lulus" : "Silahkan Coba Lagi";
+
+        System.out.println(ucapan);
+    }
+}
