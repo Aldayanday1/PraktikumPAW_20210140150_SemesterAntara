4b1b5ad64bbc48b1eb45c9649b2a707f20d700da
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-24 01:33:43 +0700

    Expression, Statement, Block

---
+++ demo2/src/main/java/com/PraktikumPAW/ExpressionStatementBlock.java
@@ -0,0 +1,31 @@
+package com.PraktikumPAW;
+
+// import java.sql.Date;
+
+public class ExpressionStatementBlock {
+    public static void main(String[] args) {
+        int value;
+        value = 10;
+        System.out.println(value = 100);
+
+        // assignment statement
+        double aValue = 8933.234;
+        // Increment statement
+        aValue++;
+        System.out.println(aValue);
+        // method invocation statement
+        System.out.println("Hello World");
+        // Object creation statement
+        // Date date = new Date();
+        // System.out.println(date);
+    }    
+}
+
+class ExpressionApp {
+    public static void main(String[] args) {
+        double aValue = 9213.21;
+        aValue++;
+        System.out.println(aValue);
+        System.out.println("Hello World");
+    }
+}
