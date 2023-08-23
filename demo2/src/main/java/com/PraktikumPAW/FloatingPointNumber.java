16d244d3c9aa926dd88b95746ec6d41c5e6f17d1
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-23 19:37:49 +0700

    Floating Point Number

---
+++ demo2/src/main/java/com/PraktikumPAW/FloatingPointNumber.java
@@ -0,0 +1,11 @@
+package com.PraktikumPAW;
+
+public class FloatingPointNumber {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        float gravity = 9.81f;
+
+        System.out.println("iniDouble: " + pi);
+        System.out.println("iniFloat: " + gravity);
+    }
+}
