811c56ee3eaf9d5482c30dce3722ac488878feab
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-24 22:10:10 +0700

    Do While Loop

---
+++ demo2/src/main/java/com/PraktikumPAW/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.PraktikumPAW;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        var counter = 1;
+
+        do {
+            System.out.println("Perulangan ke " + counter);
+            counter++;
+        } while (counter <= 50);
+    }
+}
