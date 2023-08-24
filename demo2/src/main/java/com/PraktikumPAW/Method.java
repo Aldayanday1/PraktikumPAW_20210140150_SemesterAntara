c691b93a75d61b905ec75061dce24c644d061203
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-24 23:39:00 +0700

    Method

---
+++ demo2/src/main/java/com/PraktikumPAW/Method.java
@@ -0,0 +1,10 @@
+package com.PraktikumPAW;
+
+public class Method {
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+    static void sayHelloWorld(){
+        System.out.println("Good Morning Aldy");
+    }
+}
