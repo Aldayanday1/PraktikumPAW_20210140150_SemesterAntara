13227fabc942ed0383a78eac8152e1c61a40f976
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-24 22:04:21 +0700

    For loop

---
+++ demo2/src/main/java/com/PraktikumPAW/ForLoop.java
@@ -0,0 +1,9 @@
+package com.PraktikumPAW;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(;;){
+            System.out.println("Perulangan Tanpa Henti");
+        }
+    }
+}
