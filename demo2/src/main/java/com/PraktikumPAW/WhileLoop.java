10b949d498ba9d5a678a42371838d0b35e8d5e1c
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-08-24 22:06:29 +0700

    While Loop

---
+++ demo2/src/main/java/com/PraktikumPAW/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.PraktikumPAW;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        var counter = 1;
+        while (counter <= 5){
+            System.out.println("Perulangan Ke-" + counter);
+            counter++;
+        }
+    }
+}
